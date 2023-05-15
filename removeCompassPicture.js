6a4b50164c3d1fcd3b92113453c54c6d9c16afeb
XcodeBoi <runningzoomcat@gmail.com>
2023-05-15 15:14:32 +1000

    yass

---
+++ removeCompassPicture.js
@@ -0,0 +1,10 @@
+let style = `
+.action-tip-user-photo-wrapper {
+    opacity: 0;
+}
+`;
+
+let styleInjection = document.createElement("style");
+styleInjection.innerHTML = style;
+
+document.head.appendChild(styleInjection);
